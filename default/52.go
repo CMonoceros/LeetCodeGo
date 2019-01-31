dc340934369ea9d1d735f44c4ddaf8e4d86bd907
Monoceros <zjm448172381@gmail.com>
2019-01-31 16:23:30 +0800

    2019-01-31

--- default/52.go
+++ default/52.go
@@ -4,27 +4,14 @@ import "math"
 
 // Backtracking
 
-// todo: 1
-// 12ms
-func solveNQueens(n int) [][]string {
-	var res [][]string
+// 0ms
+func totalNQueens(n int) int {
+	res := 0
 	var dfs func(loc []int, index int)
 	var checkValid func(loc []int, index int) bool
 	dfs = func(loc []int, index int) {
 		if index == n {
-			var now []string
-			for i := 0; i < n; i++ {
-				s := ""
-				for j := 0; j < n; j++ {
-					if j == loc[i] {
-						s += "Q"
-					} else {
-						s += "."
-					}
-				}
-				now = append(now, s)
-			}
-			res = append(res, now)
+			res++
 		} else {
 			for i := 0; i < n; i++ {
 				loc[index] = i
